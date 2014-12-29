01105997af21b75c9a4ab00ba74e9fdd1bcc3604
Tanay Soni <tanaysoni12@gmail.com>
2014-12-29 22:50:34 +0530

    Create MapScript.py

---
+++ MapScript.py
@@ -0,0 +1,38 @@
+###################################################
+#                                                 #
+#  A script to read location values from a csv    #
+#  file and plot them on a geographical map.      #
+#                                                 #
+#  Author - Tanay Soni                            #
+#  ts398@kent.ac.uk                               #
+#                                                 #
+###################################################
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+# Set-up map of United Kingdom using Basemap
+m = Basemap(projection='mill',llcrnrlat=50,urcrnrlat=60, llcrnrlon=-20,urcrnrlon=5,resolution='h')
+m.drawmapboundary(fill_color='aqua')
+m.fillcontinents(color='coral',lake_color='aqua')
+
+# Read accident location co-ordinates from a csv file.
+f = open('Accidents0512.csv', 'r') 
+reader = csv.reader(f, delimiter=',')
+next(reader)
+
+# Store values of the co-ordinates in lists.     
+latitudes = []
+longitudes = []
+for row in reader:
+   if(row[3] != '' and row[4] != ''):
+      longitudes.append(float(row[3]))
+      latitudes.append(float(row[4]))
+   
+# Plot co-ordinates on the Basemap
+x,y = m(longitudes, latitudes)
+m.plot(x, y, 'ro',markersize=.01)
+plt.suptitle('Locations of accidents in the UK for years 2005-2012')
+plt.savefig('map.png',dpi=400)
